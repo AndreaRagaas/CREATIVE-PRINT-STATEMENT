d883e9bf55ee21a8e5740783173e0be4c83c192e
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-04-17 08:59:57 +0800

    STATING THE STATEMENTS, FONTS, AND COLORS

--- CREATIVE PRINT STATEMENT.py
+++ CREATIVE PRINT STATEMENT.py
@@ -1,2 +1,8 @@
 import pyfiglet as pyg
 from termcolor import colored
+
+res1=pyg.figlet_format("Hi, my name is Andrea Ragaas.\nI wanted to be a", font= "term")
+print(colored((res1), "blue"))
+
+res2=pyg.figlet_format("Software Engineer", font= "speed")
+print(colored((res2), "green"))
